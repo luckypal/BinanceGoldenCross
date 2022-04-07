bc69912e307fdd5ebbd4b8e53027f1dd09f7e2fb
luckypal <bigluckypal@gmail.com>
2022-04-06 17:00:58 -0700

    Update fomular

--- index.js
+++ index.js
@@ -1,20 +1,86 @@
-const macd = require('trading-indicator').macd
-const ema = require('trading-indicator').sma
+const Binance = require('binance-api-node').default
+const sma = require('trading-indicator').sma
+const client = Binance();
 
-// let ticker = await indicators.ticker("binance", symbol, true)
+// const TIMEFRAME = '5m';
+const TIMEFRAME = '15m';
+const RATIO_DIFF_AB = 0.006;
+const RATIO_DIFF_BC = 0.01;
 
-const start = async () => {
-  // const list = await macd(12, 26, 9, "close", "binance", "BTC/USDT", "15m", false);
-  // for (let i = 0; i < 10; i ++) {
-  //   console.log(i, list[list.length - 10 + i]);
-  // }
-  
+const pastPairStatus = {};
+
+const mSleep = async (mSec) => {
+  return new Promise((resolve) => setTimeout(resolve, mSec));
+}
+
+const getFiatPairs = async (fiatCurrency) => {
+  const prices = await client.futuresPrices()
+  const pairs = Object.keys(prices)
+    .filter(pair => pair.endsWith(fiatCurrency))
+    .filter(pair => pair.replace(fiatCurrency).indexOf('USD') === -1)
+    .map(pair => pair.replace(fiatCurrency, `/${fiatCurrency}`));
+  return pairs;
+}
+
+const currentDateTime = () => {
+  return (new Date()).toLocaleString();
+}
+
+const checkGoldenCross = async (pair) => {
   const sizes = [7, 25, 99];
-  for (let i = 0; i < 3; i ++) {
-    const size = sizes[i];
-    let emaData = await ema(size, "close", "binance", "BTC/USDT", "15m", false)
-    emaData[emaData.length - 1];
+  const maData = [];
+  let isGoldenCross = null;
+
+  try {
+    for (let i = 0; i < sizes.length; i++) {
+      const size = sizes[i];
+      let smaData = await sma(size, "close", "binance", pair, TIMEFRAME, true);
+      maData.push(smaData[smaData.length - 1]);
+    }
+
+    const a = maData[0];
+    const b = maData[1];
+    const c = maData[2];
+    const diffAB = a / b - 1;
+    const diffBC = b / c - 1;
+    if (diffAB >= 0
+      && diffBC >= 0
+      && diffAB <= RATIO_DIFF_AB
+      && diffBC <= RATIO_DIFF_BC
+    ) isGoldenCross = true;
+    else if (diffAB < 0
+      && diffBC < 0
+      && diffAB >= -RATIO_DIFF_AB
+      && diffBC >= -RATIO_DIFF_BC
+    ) isGoldenCross = false;
+
+    if (isGoldenCross !== null && pastPairStatus[pair] !== isGoldenCross)
+      console.log(isGoldenCross ? 'LONG' : 'SHORT', pair);
+
+    pastPairStatus[pair] = isGoldenCross;
+  } catch (e) {
+    if (e != 'Ticker is not supported') console.log('ERROR - ', pair, e);
   }
 }
 
-start();
+const onInit = async () => {
+  console.log('MACD RADAR - FUTURES');
+  console.log('TIMEFRAME: ', TIMEFRAME);
+  console.log('DIFF: ', RATIO_DIFF_AB, RATIO_DIFF_BC);
+  const pairs = await getFiatPairs('USDT');
+  let index = 0;
+
+  while (true) {
+    if (index == 0) console.log('\n\n\n', currentDateTime());
+
+    const pair = pairs[index];
+    checkGoldenCross(pair);
+
+    index += 1;
+    if (index >= pairs.length) index = 0;
+
+    await mSleep(1000);
+  }
+};
+
+onInit();
